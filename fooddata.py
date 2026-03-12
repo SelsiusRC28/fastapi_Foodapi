0b89d30f4c00d7247cc6b976480eaeeb8cccc7cd
Erick Rozand <erickrozand@gmail.com>
2026-03-11 23:29:00 -0500

    status, tags, data, endpoints

---
+++ fooddata.py
@@ -0,0 +1,22 @@
+import json
+from pathlib import Path
+
+
+class FoodData:
+    alimentos = {}
+
+    def __init__(self):
+        data_file = Path(__file__).resolve().parent / "data" / "alimentos.json"
+        with data_file.open(encoding="utf-8") as load:
+            self.alimentos = json.load(load)
+
+    async def get_ingredients(self):
+        return self.alimentos
+
+    async def get_ingredient(self, ingrediente_id: int):
+        alimento = None
+        for item in self.alimentos["alimentos"]:
+            if item["id"] == ingrediente_id:
+                alimento = item
+                break
+        return alimento
--- main.py
+++ main.py
@@ -1,15 +1,40 @@
 from fastapi import FastAPI
+from fastapi import Response, status
+from docs import tags_metadata
+from fooddata import FoodData
 
 app = FastAPI(
-    
+    title= "FoodApi",
+    description="ApiRestFul para la gestión de alimentos y planes nutricionales",
+    version="0.0.2",
+    contact={
+        "name":"Erick 2828",
+        "url":"http://www.udemy.com"
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+    openapi_tags=tags_metadata
 )
 
 
-@app.get("/")
+@app.get('/')
 def read_root():
-    return {"Hello": "World"}
+    return { "Hola" : "Pakito"}
 
+@app.get('/ingredientes', tags=['ingredientes'])
+async def get_ingredients():
+     return await FoodData().get_ingredients()
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.get('/ingredientes/{ingrediente_id}', tags=['ingredientes'], status_code=status.HTTP_200_OK)
+async def get_ingredient(ingrediente_id : int, response : Response):
+    
+    ingredient = await FoodData().get_ingredient(ingrediente_id)
+    if ingredient :
+        return ingredient
+    else : 
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {
+            "error " : str(ingrediente_id) + " no encontrado"
+        }
